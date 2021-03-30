c2bc054291ca952e48d8419cb7af5692e2733218
Ilya Nikolskiy <aspdaspn@gmail.com>
2021-03-31 00:05:24 +0200

    Made Task 1, attempted 2

--- 07/src/main.c
+++ 07/src/main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 #define T char
+#define STRINGSIZE 81
 
 typedef struct Node {
 	T dat;
@@ -45,6 +46,14 @@ T pop(Stack *stack) {
 	return data;
 }
 
+T check(Stack *stack) {
+	if (stack -> size == 0) {
+		printf("Stack is empty\n");
+		return -1;
+	}
+	return stack -> head -> dat;
+}
+
 void printNode(Node *n) {
 	if (n == NULL) {
 		printf("[]");
@@ -66,8 +75,61 @@ void printStack(Stack *stack) {
 	printf(" Size: %d\n", stack -> size);
 }
 
+void copyList(Stack *from, Stack *to) {
+	if (from -> size == 0) {
+		printf("Empty Source, nothing to copy.\n");
+		return;
+	}
+	Node *current = from -> head;
+	do {
+	Node *tmp = (Node *) malloc(sizeof(Node));
+	if (tmp == NULL) {
+		printf("Stack overflow\n");
+		return;
+	}
+	tmp -> dat = from -> head -> dat;
+	tmp -> next = to -> head;
+	current = current -> next;
+	} while (current != NULL);
+}
+
+
+void checkBraces(Stack *stack) {
+	char ts[STRINGSIZE];
+	int i = 0;
+
+	printf("Enter test string: ");
+	fgets(ts, sizeof(ts) / sizeof(char), stdin);
+	while (ts[i] != '\0') {
+			if (ts[i] == '(' || ts[i] == '[' || ts[i] == '{' )
+				push(stack, ts[i]);
+			if (ts[i] == ')' || ts[i] == ']' || ts[i] == '}' )
+				switch(check(stack)) {
+					case '(':
+						if (ts[i] == ')')
+							pop(stack);
+						break;
+					case '[':
+						if (ts[i] == ']')
+							pop(stack);
+						break;
+					case '{':
+						if (ts[i] == '}')
+							pop(stack);
+						break;
+				}
+			i++;
+	}
+	printf("%s\n", (stack -> size == 0) ? "Braces are correct" : "Braces are wrong");
+}
+
 int main(int argc, char** args) {
-	Stack *st = (Stack *)malloc(sizeof(Stack));
+	// Task 1, Check Braces
+	Stack *br = (Stack *) malloc(sizeof(Stack));
+	init(br);
+	checkBraces(br);
+	// Task 2 Copy Dymamic List
+	Stack *st = (Stack *) malloc(sizeof(Stack));
 	init(st);
 	push(st, 'a');
 	push(st, 'b');
@@ -75,14 +137,9 @@ int main(int argc, char** args) {
 	push(st, 'd');
 	push(st, 'e');
 	printStack(st);
-	printf("%c\n", pop(st));
-	printStack(st);
-	printf("%c\n", pop(st));
-	printStack(st);
-	printf("%c\n", pop(st));
-	printStack(st);
-	printf("%c\n", pop(st));
-	printf("%c\n", pop(st));
-	printf("%c\n", pop(st));
+	Stack *cp = (Stack *) malloc(sizeof(Stack));
+	init(cp);
+	copyList(st, cp);
+	printStack(cp);
 	return EXIT_SUCCESS;
 }
