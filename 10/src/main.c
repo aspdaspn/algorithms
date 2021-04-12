01cd8663cbc3c8d457380d004b5932e8fcab0887
Ilya Nikolskiy <aspdaspn@gmail.com>
2021-04-12 21:47:37 +0200

    Made Homework 10

--- 10/src/main.c
+++ 10/src/main.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+#define ARSIZE 10
+
 int simpleHashSum(char* in) {
 	int out = 0;
 	int i = 0;
@@ -10,10 +12,40 @@ int simpleHashSum(char* in) {
 	return out;
 }
 
+int collection[ARSIZE] = { 0 };
+
+int* exchange(int* money, int size, int sum) {
+	int k = 0;
+	int subtotal = 0;
+
+	while (subtotal != sum && k < ARSIZE) {
+		for (int i = 0; i < size; ++i)
+			if (money[i] <= sum - subtotal) {
+				collection[k] = money[i];
+				++k;
+				subtotal += money[i];
+				break;
+			}
+	}
+	return collection;
+}
 
 int main(int argc, char** args) {
 	// Task 01
 	char* mystring = "Test String";
-		printf("%d\n", simpleHashSum(mystring));
+	printf("String: \"%s\" has Hash: %d\n", mystring, simpleHashSum(mystring));
+	
+	// Task 02
+	int money[] = {50, 10, 5, 2, 1};
+	int size = 5;
+	int* collection = exchange(money, size, 98);
+	int i = 0;
+	int sum = 0;
+	while (collection[i] != 0 && i < ARSIZE) {
+		printf("%d ", collection[i]);
+		sum += collection[i];
+		++i;
+	}
+	printf("\nCollection sum: %d\n", sum);
 	return EXIT_SUCCESS;
 }
