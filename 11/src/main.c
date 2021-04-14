c5f3c7dbd5cf9d83ee351615ffc09659206e40cf
Ilya Nikolsky <ilya.nikolsky@flexdsl.ch>
2021-04-14 13:58:12 +0200

    Made Task 2

--- 11/src/main.c
+++ 11/src/main.c
@@ -25,15 +25,18 @@ void clearString(char* in, int size) {
 }
 
 char* caesar(char* in, int key, bool encrypt) {
+	// Preparing the key to be positive
 	if (key < 0)
 		key = ALPHABET + key;
 
+	// Preparing the key for the decryption
 	if (!encrypt)
 		key = ALPHABET - key;
 
 	int i = 0;
 	int o = 0;
 
+	// Populating the output string with UP-case letters if required
 	while (in[i] != '\0' && i < STRSIZE) {
 		if (IS_IN_RANGE((int) in[i])) {
 			if (IS_LOW((int) in[i])) 
@@ -45,6 +48,8 @@ char* caesar(char* in, int key, bool encrypt) {
 		}
 		++i;
 	}
+
+	// adding a string terminator for a good
 	output[o] = '\0';
 	return output;
 }
@@ -53,17 +58,31 @@ char* shuffle(char* in, int key, bool encrypt) {
 	if (key < 0)
 		key = -key;
 	
+	// indexes
 	int i = 0;
 	int o = 0;
+	
+	// indicates the starting point for next pass
+	int s = 0;
+	
+	// stores the size of an input
 	int size = 0;
+
+	// stores the step variable for encryption routine
+	int step = 0;
+	
+	// a temporary array. We need it because input string can have a wrong size
 	int temp[STRSIZE] = { 0 };
 
+	// finding the size of the input string and populating the temp array
 	while (in[i] != '\0' && i < STRSIZE) {
 		temp[i] = in[i];
 		++i;
 		++size;
 	}
 
+	// If the input size / key is not an integer, increase the input size by adding 
+	// spaces at the end of the input
 	if (size / key * key < size) {
 		size = size / key * key + key;
 		while (i < size) {
@@ -72,15 +91,43 @@ char* shuffle(char* in, int key, bool encrypt) {
 		}
 	}
 	
-	for (int i = 0; i < size; ++i)
-		output[i] = temp[i];
-	++i;
-	output[i] = '\0';
-	printf("%d\n", size);
+	// Add string termination to the end of the temp array. It is for security.
+	temp[i] = '\0';
+	
+	/* 
+	 * Finding the step for encryption of for decryption
+	 * -----------
+	 * <---step-->
+	 * -----------
+	 * |1|2|3|4|5| k
+	 * |2| | | | | 
+	 * |3| | | | | e
+	 * |4| | | | | y
+	 * -----------
+	 */
+	if (encrypt)
+		step = size / key;
+	else
+		step = key;
+
+	// Reseting starting index
+	i = 0;
+
+	// Populating the output array
+	while (o < size) {
+		output[o++] = temp[i];
+		s++;
+		while (i < size - step) {
+			i = i + step;
+			output[o++] = temp[i];
+		}
+		i = s;
+	}
+
+	// adding a string terminator for a good
+	output[o] = '\0';
 	return output;
 }
-
-
 
 int main(int argc, char** args) {
 	// Task 1
@@ -92,7 +139,12 @@ int main(int argc, char** args) {
 	
 	// Task 2
 	char* cipher2 = "this is the secret message";
-	char* decrypt2 = shuffle(cipher2, 4, true);
+	char* out2 = shuffle(cipher2, 4, true);
+	printf("%s\n", out2);
+	char* decrypt2 = shuffle(out2, 4, false);
 	printf("%s\n", decrypt2);
+	char* out3 = "t cshtrsiheasetg   eism s";
+	char* decrypt3 = shuffle(out3, 4, false);
+	printf("%s\n", decrypt3);
 	return EXIT_SUCCESS;
 }
