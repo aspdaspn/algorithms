606acd3df791bdd89b3b6ba67bc9a15ab6de8d43
Ilya Nikolskiy <aspdaspn@gmail.com>
2021-04-15 22:52:39 +0200

    Made Task 1

--- 09/src/main.c
+++ 09/src/main.c
@@ -1,7 +1,10 @@
 #include "main.h"
+#include "geek.h"
 
 // Adjacency matrix
 #define MATRIXSZ 6
+#define CHAR_OFFSET_FOR_LETTERS 65
+
 int matrix[MATRIXSZ][MATRIXSZ] = {
 			{0, 1, 1, 0, 0, 0},
 			{0, 0, 0, 1, 1, 1},
@@ -11,6 +14,38 @@ int matrix[MATRIXSZ][MATRIXSZ] = {
 			{0, 0, 0, 0, 1, 0},
 };
 int visitedDepth[MATRIXSZ] = { 0 };
+
+int getUnvisitedVertex(int ver, int size) {
+	for (int i = 0; i < size; ++i) {
+		if (matrix[ver][i] == 1 && !visitedDepth[i])
+			return i;
+	}
+	return -1;
+}
+
+void depthTraversStack(int st) {
+	OneLinkList* stack = (OneLinkList*) malloc(sizeof(OneLinkList));
+	initOneLinkList(stack);
+	visitedDepth[st] = 1;
+	printf("Visited nodes sequence: ");
+	printf("%c ", 0 + CHAR_OFFSET_FOR_LETTERS);
+	pushOneLinkStack(stack, st);
+	
+	while (stack -> size != 0) {
+		int v = getUnvisitedVertex(stack -> head -> dat, MATRIXSZ);
+		if (v == -1) {
+			popOneLinkStack(stack);
+		} else {
+			visitedDepth[v] = 1;
+			printf("%c ", v + CHAR_OFFSET_FOR_LETTERS);
+			pushOneLinkStack(stack, v);
+		}
+	}
+	printf("\n");
+
+	freeOneLinkStack(stack);
+}
+
 
 void depthTravers(int st) {
 	int r;
@@ -32,14 +67,13 @@ void resetArr() {
 
 
 int main(int argc, char** args) {
+	// Task 1 Graph DTS with Stack
+	depthTraversStack(0);
+	resetArr();
+	printf("Visited sequence array: ");
 	depthTravers(0);
-	resetArr();
 	printf("\n");
-	depthTravers(2);
 	resetArr();
-	printf("\n");
-	depthTravers(1);
-	resetArr();
-	printf("\n");
+
 	return EXIT_SUCCESS;
 }
